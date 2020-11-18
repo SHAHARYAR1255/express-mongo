5695e1b61bdc1876b0be0cd27222df30b4bc9b72
SHAHARYAR MALIK <shaharyar.malik2000@gmail.com>
2020-11-19 02:25:38 +0500

    passport with session

--- app.js
+++ app.js
@@ -4,27 +4,39 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 const Dishes = require("./models/dishes");
 
+// function auth (req, res, next) {
+//     console.log(req.session);
 
-function auth (req, res, next) {
-    console.log(req.session);
+//   if(!req.session.user) {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       return next(err);
+//   }
+//   else {
+//     if (req.session.user === 'authenticated') {
+//       next();
+//     }
+//     else {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
+function auth(req, res, next) {
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+  if (!req.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    next(err);
+  } else {
+    next();
   }
 }
 const url = "mongodb://localhost:27017/conFusion";
@@ -51,7 +63,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 //auth setup
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(cookieParser("12345-67890-09876-54321"));
 
 app.use(auth);
 app.use(logger("dev"));
@@ -59,6 +71,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
